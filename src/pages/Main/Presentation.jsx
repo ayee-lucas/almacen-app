1cdb773f6395a37a0f055753901763df2f69ea80
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-04-22 14:32:40 -0600

    Added Presentation Page with image

---
+++ src/pages/Main/Presentation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Presentation = () => {
+    return (
+        <div className='bg-presentation w-full h-screen bg-cover bg-center'>
+            <div className='absolute bg-black left-5 top-3 bottom-3 right-5 opacity-70' />
+            
+           
+        </div>
+    );
+}
+
+export default Presentation;
