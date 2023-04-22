c38e6ab2bd353ca52e6a80db3f6d53303d091d05
falegria-2018246 <falegria-2018246@kinal.edu.gt>
2023-04-21 18:01:04 -0600

    Se creo la conexión a la BD y modifico Index.js

--- src/pages/api/configs/mongo.js
+++ src/pages/api/configs/mongo.js
@@ -1,29 +1,17 @@
-const mongoose = require('mongoose');
+'use strict'
+import mongoose from 'mongoose';
 
-
-exports.dbConnection = async () => {
+const { MONGODB_URI } = process.env;
+export const connection = async()=> {
     try {
-        const uriMongo = `${process.env.MONGODB_URI}`;
-        mongoose.set('maxTimeMS', 2500);
-        mongoose.set('strictQuery', false)
-        await mongoose.connect(uriMongo);
-        console.log('Connected to DB');
-    } catch (err) {
-        console.log(err)
-    }
+        await mongoose.connect(MONGODB_URI, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
+        console.log('Conexión exitosa a la base de datos');
+      } catch (err) {
+        console.error('Error al conectar a la base de datos', err);
+      };
 }
 
-const conn = {
-    isConnected: false
-}
-
-exports.dbConnection = async ()=>{
-    if(conn.isConnected) return;
-    const db = await connect(process.env.MONGODB_URI);
-    conn.isConnected = db.connection[0].readyState;
-    console.log(db.connection.db.almacenadora5)
-}
-
-connection.on('connected',()=>{
-    console.log('MongoDb is connected');
-})
+export default connection;
