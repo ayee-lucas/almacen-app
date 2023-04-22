5bd9532814ca6dbed1b6e5a556611a78d8bba979
falegria-2018246 <falegria-2018246@kinal.edu.gt>
2023-04-21 18:04:03 -0600

    Se creo conexion a BD y modifico Index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import  connection  from './api/configs/mongo'
+import  {connection}  from './api/configs/mongo'
 
 const inter = Inter({ subsets: ['latin'] })
 connection()
