f560d7a130e61c36347d1a7e7a620f448a51e53f
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-04-24 10:52:14 -0600

    Responsive Improved

--- src/pages/Main/Presentation.jsx
+++ src/pages/Main/Presentation.jsx
@@ -4,19 +4,34 @@ import { AiOutlineRight, AiOutlineDown } from "react-icons/ai";
 
 const Presentation = () => {
   return (
-    <div className="flex justify-center items-center bg-presentation w-full h-screen bg-cover bg-center bg-fixed">
-      <div className="absolute bg-black left-5 top-3 bottom-3 right-5 opacity-70" />
-      <div className="z-[2]">
+    <div className="bg-presentation w-full h-screen bg-cover bg-center bg-fixed">
+      <div className="absolute bg-black left-5 top-3 bottom-3 right-5 opacity-70
+                      max-sm:left-0  max-sm:top-0 max-sm:bottom-0 max-sm:right-0 max-sm:opacity-90 max-sm:bg-gradient-to-b max-sm:from-black max-sm:via-black gradient 
+      
+      " />
+      <div className="flex items-center w-full h-full sm:justify-center">
+
+
+      <div className=" z-[2] max-sm:ml-6">
         <div className="underline-container">
-          <h1 className=" underline-text text-white ropa-sans tracking-widest text-center text-[140px] font-bold">
+          <h1 className="underline-text text-white ropa-sans tracking-widest text-center text-[140px] font-bold max-sm:hidden">
             BLACKBOX
           </h1>
-          <div className="underline" />
-          <h3 className="underline-label text-white jetbrains-mono text-center text-[15px]">
+
+          <div className="underline max-sm:hidden" />
+          <h3 className="underline-label max-sm:hidden text-white jetbrains-mono text-center text-[15px]">
             Almacenamos tu exito, cuidamos tus productos
           </h3>
         </div>
-        <div className="z-[2] w-full flex justify-center items-center mt-20">
+
+        {/** Mobile Title  */}
+
+        <div className="underline-text text-white jetbrains-mono text-[60px] sm:hidden">
+          <h1>BLACK</h1>
+          <h1>BOX</h1>
+        </div>
+
+        <div className="z-[2] max-sm:absolute max-sm:left-[30%] mt-20 max-sm:ml-4">
           <Link
             href={"/"}
             className="bg-black py-3 px-10 rounded-[10px] relative flex justify-between items-center"
@@ -25,9 +40,14 @@ const Presentation = () => {
             <AiOutlineRight className="text-white absolute right-1 " />
           </Link>
         </div>
+
+
+      </div>
         <div className="flex justify-center">
           <div className="absolute bottom-9">
-            <h1 className="text-white text-sm tracking-widest jetbrains-mono pb-1">Mas Informacion</h1>
+            <h1 className="text-white text-sm tracking-widest jetbrains-mono pb-1">
+              Mas Informacion
+            </h1>
             <AiOutlineDown className="text-white absolute left-[45%]" />
           </div>
         </div>
