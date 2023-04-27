1d3209354eee6e3b8d72a5969d44b8b2e7386d45
jmorales <jmorales-2018317>
2023-04-26 23:32:36 -0600

    Finalización del diseño de AboutPage

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -4,24 +4,29 @@ const AboutPage = () => {
   return (
     <div className="h-screen w-full bg-presentationAboutPage bg-cover max-sm:bg-center max-lg:h-full">
       <div className="relative h-full w-full">
-        <div className="absolute transition-all bg-black inset-0 opacity-70 max-sm:opacity-90 max-sm:bg-gradient-to-b max-sm:from-black max-sm:via-black gradient" />
+        <div
+          className="absolute transition-all bg-black inset-0 opacity-70 
+                        max-sm:opacity-70 max-sm:bg-gradient-to-b max-sm:from-black max-sm:via-black gradient"
+        />
+        <div className="absolute transition-all bg-black inset-0 opacity-0 max-sm:opacity-60" />
         <div className="underline-container">
           <h1
             className="pl-20 pt-20 underline-text text-white jetbrains-mono tracking-tighter text-[80px] font-bold
-                           max-sm:text-[50px] max-sm:pl-5 max-sm:pt-6 "
+                       max-sm:text-[50px] max-sm:pl-8 max-sm:pt-8 max-lg:text-[70px]"
           >
             About Us
+            <div className="bg-white w-80 h-px max-sm:ml-0 max-sm:w-40 max-lg:w-60" />
           </h1>
-          <div className="underlineAboutPage" />
         </div>
-        <div className="flex justify-evenly p-20 relative text-[18px]
-                          max-sm:grid max-sm:grid-rows-3 max-sm:gap-1 max-sm:p-0 max-sm:px-2 max-sm:py-5 max-sm:text-[15px]"
+        <div
+          className="relative flex justify-between p-20 pb-16 text-[20px] max-lg:pt-16 max-lg:pb-32
+                     max-sm:-pb-10 max-sm:grid max-sm:grid-rows-2 max-sm:gap-1 max-sm:p-0 max-sm:px-3 max-sm:py-8 max-sm:pb-32 max-sm:text-[15px]"
         >
-          <div className="text-justify pr-32 max-lg:pr-10 max-sm:px-4">
-            <h2 className=" text-white k2d text-[40px]">
+          <div className="text-justify pr-36 max-lg:pr-12 max-sm:px-4">
+            <h2 className=" text-white k2d text-[40px] max-lg:text-[28px]">
               Storage Solutions
             </h2>
-            <p className=" text-white tracking-wider mt-5">
+            <p className=" text-white tracking-wider mt-5 max-lg:text-[16px] max-lg:mt-7">
               Somos una empresa almacenadora comprometida en ofrecer soluciones
               de almacenamiento seguras y confiables para nuestros clientes. Con
               años de experiencia, comprendemos la importancia de la seguridad y
@@ -30,11 +35,11 @@ const AboutPage = () => {
               sus bienes.
             </p>
           </div>
-          <div className="text-justify max-lg:pl-10 max-sm:px-4 max-sm:mt-5">
-            <h2 className=" text-white k2d text-[40px]">
+          <div className="text-justify max-sm:px-4 max-sm:mt-4">
+            <h2 className=" text-white k2d text-[40px] max-lg:text-[28px]">
               Offers
             </h2>
-            <p className=" text-white tracking-wider mt-5">
+            <p className=" text-white tracking-wider mt-5 max-lg:text-[16px] max-lg:mt-7">
               Ofrecemos un amplio rango de servicios de almacenamiento y
               trabajamos con cada cliente para personalizar nuestros servicios y
               adaptarnos a sus necesidades específicas. Nos esforzamos por
