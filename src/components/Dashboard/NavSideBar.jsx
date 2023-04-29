50d209285552a2e1c69e02eba503dbc0353b8967
jmorales-2018317 <jmorales-2018317@kinal.edu.gt>
2023-04-29 14:31:46 -0600

    Sidebar Finished

--- src/components/Dashboard/NavSideBar.jsx
+++ src/components/Dashboard/NavSideBar.jsx
@@ -1,16 +1,13 @@
 import React from "react";
-import { AiOutlineHome } from "react-icons/ai";
 
 const NavSideBar = ({icon, name}) => {
   return (
     <>
-      <div className="relative flex items-center pt-3">
-        <AiOutlineHome className="text-white" />
-        <a href="" className="pl-3">
-          {name}
-        </a>
+      <div className="relative flex items-center pt-3 max-sm:justify-center">
+        {icon}
+        <button type="button" className="pl-3">{name}</button>
       </div>
-      <div className="mt-5 opacity-20 bg-white w-50 h-px max-sm:ml-0 max-sm:w-40 max-lg:w-60" />
+      <div className="mt-5 opacity-20 bg-white w-50 h-px max-sm:mx-10" />
     </>
   );
 };
--- src/components/Dashboard/SideBar.jsx
+++ src/components/Dashboard/SideBar.jsx
@@ -1,28 +1,39 @@
 import React from "react";
 import NavSideBar from "./NavSideBar";
-import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
+import {AiOutlineHome,AiOutlineDatabase,AiOutlineShop,AiOutlineUser,AiOutlineUserDelete, AiOutlineClose} from "react-icons/ai";
 import Image from "next/image";
 import logo from "../../assets/Logos/blackboxDark.png";
 
 const SideBar = () => {
-    const array = ["Home", "Bodega", "Sucursales", "Clientes"];
   return (
     <>
-      <nav className="absolute bg-neutral-950 p-6 z-0 w-[350px] h-full">
+      <nav className="absolute bg-neutral-950 p-6 z-0 w-[350px] h-full max-sm:w-full">
         <div className="relative flex justify-between items-center">
-          <div className="relative flex items-center">
+          <div className="flex items-center">
             <Image src={logo} width={50} height={50} alt="logo" />
-            <h1 className="text-white jetbrains-mono text-[20px] font-medium">
+            <h1 className="text-white jetbrains-mono ml-1 text-[22px] font-medium">
               BLACKBOX
             </h1>
           </div>
-          <AiOutlineMenu className="text-white text-[30px]" />
+          <AiOutlineClose onClick={""} className="text-white text-[30px]" />
         </div>
-        <div className="p-2 pt-20 text-white k2d font-light text-[20px]">
-            <NavSideBar name={'Home'}/>
-            <NavSideBar name={'Home'}/>
-            <NavSideBar name={'Home'}/>
-            <NavSideBar name={'Home'}/>
+        <div className="h-[95%] p-2 text-white k2d font-light text-[20px]">
+
+          <div className="pt-20">
+            <NavSideBar name={"Home"} icon={<AiOutlineHome />} />
+            <NavSideBar name={"Bodega"} icon={<AiOutlineDatabase />} />
+            <NavSideBar name={"Sucursal"} icon={<AiOutlineShop />} />
+            <NavSideBar name={"Clientes"} icon={<AiOutlineUser />} />
+          </div>
+
+          <div className="absolute bottom-10">
+            <div className="mt-5 opacity-20 bg-white w-50 h-px" />
+            <div className="flex items-center">
+              <AiOutlineUserDelete/>
+              <button type="button" className="pl-3">Log Out</button>
+            </div>
+          </div>
+
         </div>
       </nav>
     </>
