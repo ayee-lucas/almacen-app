b4a628d738e35cb351c712858be8a3cf0c35d14d
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-04 13:23:18 -0600

    Login

---
+++ src/pages/api/auth/jwt.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken"
+
+
+const generateToken = async(data) => {
+    try {
+        const payload = {
+            uui: data.id,
+            name: data.name,
+            email: data.email,
+            role: data.role,
+            username:  data.username,
+            iat: Math.floor(Date.now()/ 1000),
+            exp:  Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 30 //30 dias
+        }
+
+        return jwt.sign(payload, `${process.env.JWT_SECRET}`);
+
+        
+    } catch (err) {
+        console.log(err)
+        return err
+    }
+
+}
+
+export default generateToken;
