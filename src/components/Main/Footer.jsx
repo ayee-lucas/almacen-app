e1f9985aea91f09759ee2c875ffd057862723f25
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-04 05:40:10 -0600

    changed links to local storage

--- src/components/Main/Footer.jsx
+++ src/components/Main/Footer.jsx
@@ -96,8 +96,7 @@ const Footer = () => {
           <div className="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
             <a
               href="#"
-              className="text-gray-500 hover:text-gray-900 "
-            >
+              className="text-gray-500 hover:text-gray-900 " >
               <svg
                 className="w-5 h-5"
                 fill="currentColor"
