3e6480cda9819a19392608d783c824d49c08f633
falegria-2018246 <falegria-2018246@kinal.edu.gt>
2023-05-04 14:33:52 -0600

    Cambio en cellars

--- src/pages/api/Controllers/Cellars/Cellars.js
+++ src/pages/api/Controllers/Cellars/Cellars.js
@@ -16,6 +16,15 @@ export default async function handler(req, res) {
                 return res.status(500).json({ error: err.message })
             }
 
+            case "GET":
+            try {
+                const cellars = await Cellars.find();
+                if (!cellars) return res.status(404).send({ message: 'Cellars not found' });
+                return res.status(200).json({ msg: cellars, success: true });
+            } catch (err) {
+                return res.status(500).json({ error: err.message })
+            }
+
+
 }
 }
-//Sin validaciones
