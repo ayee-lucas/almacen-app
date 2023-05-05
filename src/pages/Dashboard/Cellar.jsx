9821b1f0a37fd4975d1e4889e3f2b144a15a3a3b
jmorales <jmorales-2018317@kinal.edu.gt>
2023-05-05 02:42:00 -0600

    Cellar Crud created

---
+++ src/pages/Dashboard/Cellar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Navbar from "@/components/Navbar";
+import CellarTable from "@/components/Cellar/CellarTable";
+
+const Cellar = () =>{
+    return(
+        <>
+            <div className="dark:bg-gray-800 h-full">
+                <Navbar/>
+                <div className="dark:bg-gray-800">
+                    <div className="text-white dark:bg-gray-800 h-20 jetbrains-mono font-semibold text-[35px] p-5">
+                        <h1 className="flex justify-center">CELLAR</h1>
+                    </div>
+                    <div className="p-10"> 
+                        <CellarTable/>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Cellar;
