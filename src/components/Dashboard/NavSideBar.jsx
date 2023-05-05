2688bc3d78f77a4299594c208094dfe5d37415f3
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-05 02:43:44 -0600

    home

--- src/components/Dashboard/NavSideBar.jsx
+++ src/components/Dashboard/NavSideBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+
 const NavSideBar = ({icon, name}) => {
   return (
     <>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,21 +1,34 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState, createContext } from "react";
 import Home from "@/components/Dashboard/Home";
 import SideBar from "@/components/Dashboard/SideBar";
 import Navbar from "@/components/Navbar";
 import { useSession, signIn, signOut } from "next-auth/react";
 
+export const DashboardContext = createContext();
+
 const Dashboard = () => {
+  const [nav, setNav] = useState(false);
 
-  const { status } = useSession();
+  const handleNav = () => {
+    setNav(!nav);
+    console.log(nav);
+  };
 
+  const value = {
+    nav,
+    handleNav
+  };
 
   return (
-   <div className="bg-[E0E0E0] h-screen">
-        <Navbar/>
-        <Home/>
-      </div>
-
+    <>
+      <DashboardContext.Provider value={value}>
+        <div className="bg-[E0E0E0] h-screen">
+          <SideBar />
+          <Navbar />
+          <Home />
+        </div>
+      </DashboardContext.Provider>
+    </>
   );
 };
 export default Dashboard;
-
