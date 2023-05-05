789f6504314d146fbc54c7442d1f2423a7d81890
falegria-2018246 <falegria-2018246@kinal.edu.gt>
2023-05-04 22:20:20 -0600

    ADMIN ISSUES

--- src/pages/api/Controllers/Cellars/Cellars.js
+++ src/pages/api/Controllers/Cellars/Cellars.js
@@ -1,21 +1,28 @@
 import { connectdb } from "../../configs/mongo";
 import Cellars from "../../models/Cellars";
+import { getSession } from "next-auth/react";
 
 connectdb();
 
 export default async function handler(req, res) {
-    const { method, body, query: { id } } = req;
-    switch (method) {
-        case "POST":
-            try {
-                const newCellar = new Cellars(body)
-                const saveCellar = await newCellar.save()
-                return res.status(201).json(saveCellar);
-            } catch (err) {
-                console.error(err)
-                return res.status(500).json({ error: err.message })
-            }
+  const { method, body, query: { id } } = req;
+  const session = await getSession({ req });
 
+  switch (method) {
+    case "POST":
+      try {
+        if (session.logged.role !== 'admin') {
+            return res.status(403).json({ message: 'You do not have permission to perform this action' })
+          }
+        const newCellar = new Cellars(body)
+        const saveCellar = await newCellar.save()
+        return res.status(201).json(saveCellar);
+      } catch (err) {
+        console.error(err)
+        return res.status(500).json({ error: err.message })
+      }
+
+    default:
+      return res.status(405).json({ message: `Method ${method} Not Allowed` });
+  }
 }
-}
-//Sin validaciones
