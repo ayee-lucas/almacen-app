45b87e3c42198fc60aa40eb8e528a12378133359
pvasquez-2018044 <pvasquez-2018044@kinal.edu.gt>
2023-05-04 22:51:34 -0600

    Se agrego el metodo GET en Cellars

--- src/pages/api/Controllers/Cellars/Cellars.js
+++ src/pages/api/Controllers/Cellars/Cellars.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
         case "POST":
             try {
                 const newCellar = new Cellars(body)
+                body.service == null;
                 const saveCellar = await newCellar.save()
                 return res.status(201).json(saveCellar);
             } catch (err) {
@@ -16,6 +17,15 @@ export default async function handler(req, res) {
                 return res.status(500).json({ error: err.message })
             }
 
+            case "GET":
+            try {
+                const cellars = await Cellars.find();
+                if (!cellars) return res.status(404).send({ message: 'Cellars not found' });
+                return res.status(200).json({ msg: cellars, success: true });
+            } catch (err) {
+                return res.status(500).json({ error: err.message })
+            }
+
+
 }
 }
-//Sin validaciones
