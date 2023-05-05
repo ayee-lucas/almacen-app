76c15fbe4804eebad71018f677076aecff8473b8
alopez-2018459 <alopez-2018459@kinal.edu.gt>
2023-05-04 20:41:49 -0600

    Fixed Page redirecting and added validation router

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Home from "@/components/Dashboard/Home";
 import SideBar from "@/components/Dashboard/SideBar";
 import Navbar from "@/components/Navbar";
+import { useSession, signIn, signOut } from "next-auth/react";
+
 
 const Dashboard = () => {
+
+  const { status } = useSession();
+
+    useEffect(() => {
+    if (status === "unauthenticated") {
+      signIn("credentials");
+    }
+  }, [status]);
+
   return (
-      <div className="bg-[E0E0E0] h-screen">
+   <div className="bg-[E0E0E0] h-screen">
         <Navbar/>
         <Home/>
       </div>
   );
 };
-
 export default Dashboard;
